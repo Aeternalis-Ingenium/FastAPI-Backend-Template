69d2487aafa65e4e63ad37e08d67f9b07aa8898e
lana freitas <lanadeoliveira@gmail.com>
2023-01-21 13:52:04 -0300

    working

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,45 +1,11 @@
-import fastapi
-import uvicorn
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
-from src.api.endpoints import router as api_endpoint_router
-from src.config.events import execute_backend_server_event_handler, terminate_backend_server_event_handler
-from src.config.manager import settings
+backend_app = FastAPI()
 
+@backend_app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-def initialize_backend_application() -> fastapi.FastAPI:
-    app = fastapi.FastAPI(**settings.set_backend_app_attributes)  # type: ignore
-
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.ALLOWED_ORIGINS,
-        allow_credentials=settings.IS_ALLOWED_CREDENTIALS,
-        allow_methods=settings.ALLOWED_METHODS,
-        allow_headers=settings.ALLOWED_HEADERS,
-    )
-
-    app.add_event_handler(
-        "startup",
-        execute_backend_server_event_handler(backend_app=app),
-    )
-    app.add_event_handler(
-        "shutdown",
-        terminate_backend_server_event_handler(backend_app=app),
-    )
-
-    app.include_router(router=api_endpoint_router, prefix=settings.API_PREFIX)
-
-    return app
-
-
-backend_app: fastapi.FastAPI = initialize_backend_application()
-
-if __name__ == "__main__":
-    uvicorn.run(
-        app="main:backend_app",
-        host=settings.SERVER_HOST,
-        port=settings.SERVER_PORT,
-        reload=settings.DEBUG,
-        workers=settings.SERVER_WORKERS,
-        log_level=settings.LOGGING_LEVEL,
-    )
+@backend_app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
